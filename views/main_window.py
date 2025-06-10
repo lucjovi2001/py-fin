38ce1b9c0df2f0a304fb050317c038b25c50450b
lvidetto <lucas.videtto@jt4llc.com>
2025-06-10 08:05:12 -0700

    Show and resize main window

---
+++ views/main_window.py
@@ -0,0 +1,11 @@
+from PyQt6.QtWidgets import QMainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.__init_ui()
+
+    def __init_ui(self):
+        self.resize(720, 720)
